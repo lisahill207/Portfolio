bb21bc1a68a5a638d8ebfb3ea1b18eebc63fd39c
Lisa Hill <lisahill207@gmail.com>
2024-02-03 18:21:16 -0500

    convert plan to react

---
+++ Projects/Plan/plan/src/TUheader.js
@@ -0,0 +1,86 @@
+import React from "react";
+import tulogo from "./Images/tulogo.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faLaptop } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+function TUheader() {
+  return (
+    <div className="tuNavBars">
+      <div className="upperNavBar">
+        <div className="navigation">
+          <ul className="tuNavItems">
+            <li className="tuNavItem">
+              Students
+              <FontAwesomeIcon icon={faCaretDown} />
+            </li>
+            <li className="tuNavItem">
+              Parents
+              <FontAwesomeIcon icon={faCaretDown} />
+            </li>
+            <li className="tuNavItem">
+              Alumni
+              <FontAwesomeIcon icon={faCaretDown} />
+            </li>
+            <li className="tuNavItem">
+              Faculty & Staff
+              <FontAwesomeIcon icon={faCaretDown} />
+            </li>
+            <li className="tuNavItem">
+              Business
+              <FontAwesomeIcon icon={faCaretDown} />
+            </li>
+          </ul>
+        </div>
+        <div className="otherLinks">
+          <ul className="tuNavItems">
+            <li className="tuNavItem">
+              <FontAwesomeIcon icon={faHeart} /> Give to TU
+            </li>
+            <li className="tuNavItem">
+              <FontAwesomeIcon icon={faCalendar} />
+              Calendars & Events
+            </li>
+            <li className="tuNavItem">
+              <FontAwesomeIcon icon={faLaptop} />
+              Directories
+            </li>
+            <li className="tuNavItem" id="tuSearch">
+              Search
+              <FontAwesomeIcon icon={faMagnifyingGlass} />{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="lowerNavBar">
+        <div className="leftGraphics">
+          <div className="parent">
+            <div id="parallelogram"></div>
+            <div className="child">
+              <img
+                src={tulogo}
+                className="tuLogo"
+                alt="Logo for Towson University"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="rightLinks">
+          <ul className="lowerLinks">
+            <li className="lowerLink">About TU</li>
+            <li className="lowerLink">Academics</li>
+            <li className="lowerLink">Admissions & Aid</li>
+            <li className="lowerLink">Student Life</li>
+            <li className="lowerLink">Campus & Community</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TUheader;
---
+++ Projects/Plan/plan/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./plan.css";
+
+import TUheader from "./TUheader";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<TUheader />);
